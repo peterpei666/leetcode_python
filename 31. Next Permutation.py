879a6f42dc9f1d5d30443b23959feb6fd673db0b
Peter_Pei <mmiaomiao140@gmail.com>
2025-10-01 22:58:03 +0800

    Add files via upload

---
+++ 31. Next Permutation.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i == -1:
+            nums.reverse()
+            return False
+        j = n - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = nums[i + 1:][::-1]
