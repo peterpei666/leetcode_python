49773b3770c67c0fe30a3e9c185d646813a90acb
Peter_Pei <mmiaomiao140@gmail.com>
2026-01-25 23:38:30 +0800

    Add files via upload

---
+++ 1984. Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return min(nums[i + k - 1] - nums[i] for i in range(len(nums) - k + 1))
